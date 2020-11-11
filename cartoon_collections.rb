94df26c4ab236f017a607db538731b0ff6380883
Alison Williams <apw5011@gmail.com>
2020-11-11 00:02:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find(cheese.array)
 end
